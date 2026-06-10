69c694a0a5bdcc5642459f45bd856ec3cca30e03
korish181 <oliinyk.serhii@e-u.edu.ua>
2026-06-10 08:19:48 +0200

    Практична 3

---
+++ PR3.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    const result = [];
+
+    for (let i = 0; i < grades.length; i++) {
+        let grade = grades[i];
+
+        if (grade >= 38) {
+            const nextMultiple = Math.ceil(grade / 5) * 5;
+
+            if (nextMultiple - grade < 3) {
+                grade = nextMultiple;
+            }
+        }
+
+        result.push(grade);
+    }
+
+    return result;
+}
+
+function main() {
+    const ws = process.stdout;
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
